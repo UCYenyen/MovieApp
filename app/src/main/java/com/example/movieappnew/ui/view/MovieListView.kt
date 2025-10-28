b228c68cd02c1ac1032bed28fc8e49fa4e57aafa
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-10-28 16:41:27 +0700

    fix bug gak ke refresh klau like di desc

--- app/src/main/java/com/example/movieappnew/ui/view/MovieListView.kt
+++ app/src/main/java/com/example/movieappnew/ui/view/MovieListView.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -16,6 +17,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -24,8 +29,15 @@ import com.example.movieappnew.ui.viewmodel.MovieListViewModel
 
 @Composable
 fun MovieListView(viewModel: MovieListViewModel = viewModel(), navController: NavController = rememberNavController()) {
-    val movies by viewModel.movies.collectAsState()
+//    val movies by viewModel.movies.collectAsState()
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(lifecycleOwner) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+            viewModel.loadMovie() // runs once per resume
+        }
+    }
 
+    val movies by viewModel.movies.collectAsStateWithLifecycle()
     Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
 //        Text("Movie List", style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold))
         LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(4.dp)) {
